76c263e3fcf2b15ca14bfd3305c92cbbb752e939
Carter G <carterguthrie94@yahoo.com>
2015-01-23 01:10:40 -0500

    updated files

---
+++ inc/mobilenav.inc.php
@@ -0,0 +1,8 @@
+			<li class="main-nav nav-a"><a href="">HOME</a></li>
+			<li class="main-nav nav-a"><a href="">GALLERY</a></li>	
+			<li class="main-nav nav-a"><a href="">AB0UT US</a></li>	
+			<li class="main-nav nav-a"><a href="">CONTACT</a></li>
+			<li class="main-nav nav-a"><a href="">FRIENDZ</a></li>	
+			<li class="main-nav nav-a"><a href="">NEWS</a></li>	
+			<li class="main-nav nav-a"><a href="http://nothingextravagant.bigcartel.com/" target="_blank">STORE</a></li>
+	
